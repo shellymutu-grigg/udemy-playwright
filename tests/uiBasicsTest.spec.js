edce8409d28c7e69de10f31367911fadd443bc52
shellymutu-grigg <shellymutugrigg@gmail.com>
2024-01-11 16:30:21 +1300

    Section 03 Lesson 12: Add course work

--- tests/uiBasicsTest.spec.js
+++ tests/uiBasicsTest.spec.js
@@ -1,26 +1,39 @@
 const {test, expect} = require('@playwright/test');
 
-// Run in headed mode: npx playwright test --headed 
+// Run in non headless mode: npx playwright test --headed 
+// Run playwright UI: npx playwright test --ui 
+// Run playwright UI: npx playwright test
 
-test('Page context playwright test', async ({browser}) =>
+test('A page context playwright test', async ({browser}) =>
 {
     // Remember that JS is asynchronous not sequential
     // Create Chrome instance or inject page variable in fat arrow function
     let context = await browser.newContext();
     let page = await context.newPage();
-    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.goto('https://www.google.com/');
+
+    // Print the page title
+    await console.log('Page title: ', await page.title());
+
+    await expect(page).toHaveTitle('Google');
 });
 
 // test.only to run a single test
 test('Injected page variable playwright test', async ({browser, page}) =>
 {
     // Remember that JS is asynchronous not sequential
-    await page.goto('https://www.google.com/');
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 
     // Print the page title
-    console.log(await page.title());
-    await expect(page).toHaveTitle("Google");
+    await console.log('Page title: ', await page.title());
+
+    await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
     await page.locator('#username').fill('rahulshetty');
     await page.locator("[type='password']").fill('learning');
     await page.locator('#signInBtn').click();
+
+    // Print out error message text
+    console.log('Error text:', await page.locator("[style*='block']").textContent());
+
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect username');
 });
