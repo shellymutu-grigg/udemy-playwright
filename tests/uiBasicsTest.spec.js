fe865a274dec050594b2c3ca275e9f70bf9e28ee
shellymutu-grigg <shellymutugrigg@gmail.com>
2024-01-11 15:05:33 +1300

    Section 02: Add course work details

---
+++ tests/uiBasicsTest.spec.js
@@ -0,0 +1,23 @@
+const {test, expect} = require('@playwright/test');
+
+// Run in headed mode: npx playwright test --headed 
+
+test('Page context playwright test', async ({browser}) =>
+{
+    // Remember that JS is asynchronous not sequential
+    // Create Chrome instance or inject page variable in fat arrow function
+    let context = await browser.newContext();
+    let page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
+
+// test.only to run a single test
+test('Injected page variable playwright test', async ({browser, page}) =>
+{
+    // Remember that JS is asynchronous not sequential
+    await page.goto('https://www.google.com/');
+
+    // Print the page title
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+});
