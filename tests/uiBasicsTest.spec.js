24f648d58b9b63b1bee38a3e59650e3bbed707b3
shellymutu-grigg <shellymutugrigg@gmail.com>
2024-01-11 17:03:38 +1300

    Section 03 Lesson 14: Add course work

--- tests/uiBasicsTest.spec.js
+++ tests/uiBasicsTest.spec.js
@@ -13,22 +13,23 @@ test('A page context playwright test', async ({browser}) =>
     await page.goto('https://www.google.com/');
 
     // Print the page title
-    await console.log('Page title: ', await page.title());
+    console.log('Page title: ', await page.title());
 
     await expect(page).toHaveTitle('Google');
 });
 
 // test.only to run a single test
-test('Injected page variable and check for error message playwright test', async ({browser, page}) =>
+test('Injected page variable, check for login error message and then login playwright test', async ({browser, page}) =>
 {
     let username = page.locator('#username');
     let password = page.locator("[type='password']");
     let signInBtn = page.locator('#signInBtn')
+    let cardTitles = page.locator(".card-body a");
     // Remember that JS is asynchronous not sequential
     await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 
     // Print the page title
-    await console.log('Page title: ', await page.title());
+    console.log('Page title: ', await page.title());
 
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
     await username.fill('rahulshetty');
@@ -44,6 +45,36 @@ test('Injected page variable and check for error message playwright test', async
     await password.fill('learning');
     await signInBtn.click();
 
-    // Print the text of the second card-body element
-    await console.log('Card-body text ', await page.locator(".card-body a").nth(1).textContent());
+    // Print the text of the second card title
+    console.log('Card title text ', await cardTitles.nth(1).textContent());
+
+    // allTextContent() method will not wait for element load like at L#49
+    let allCardTitles = await cardTitles.allTextContents();
+    console.log('All card titles text ', allCardTitles);
+});
+
+test.only('Login', async ({browser, page}) =>
+{
+    let username = page.locator('#userEmail');
+    let password = page.locator('#userPassword');
+    let loginBtn = page.locator('#login')
+    let cardTitles = page.locator(".card-body b");
+
+    // Remember that JS is asynchronous not sequential
+    await page.goto('https://rahulshettyacademy.com/client');
+
+    // Print the page title
+    console.log('Page title: ', await page.title());
+
+    await expect(page).toHaveTitle("Let's Shop");
+    await username.fill('shellymutugrigg@gmail.com');
+    await password.fill('gazxHSwK$oBbd*c43t4S');
+    await loginBtn.click();
+
+    // Print the text of the first card title
+    console.log('Card title text ', await cardTitles.first().textContent());
+
+    // allTextContent() method will not wait for element load like at L#49
+    let allCardTitles = await cardTitles.allTextContents();
+    console.log('All card titles text ', allCardTitles);
 });
