a69e806a61d3452141029edfda8a76b3064bb3e8
shellymutu-grigg <shellymutugrigg@gmail.com>
2024-01-13 17:38:18 +1300

    Section 09 Lesson 40: Inject token into cookies to by-pass login

--- tests/section09Test.spec.js
+++ tests/section09Test.spec.js
@@ -8,10 +8,32 @@
  * Generate code: npx playwright codegen htps://www.google.com
 */
 
-const { test, expect } = require('@playwright/test');
+const { test, expect, request } = require('@playwright/test');
+
+const payLoad = {
+    userEmail: process.env.username_rahulshetty,
+    userPassword: process.env.password_rahulshetty,
+};
+let token;
+
+test.beforeAll( async () => {
+    const apiContext = await request.newContext();
+    const loginResponse = await apiContext.post('https://rahulshettyacademy.com/api/ecom/auth/login', 
+    {
+        data: payLoad,
+    });
+    await expect(loginResponse.ok()).toBeTruthy();
+    const responseJson = await loginResponse.json();
+    token = responseJson.token;
+    console.log('token:', token)
+});
 
 test('Playwright API testing', async ({ page }) =>
 {
+    // Inject token into cookies to prevent needing to login
+    await page.addInitScript(value => {
+        window.localStorage.setItem('token', value)
+    }, token)
     await page.goto('https://rahulshettyacademy.com/client');
     
 });
