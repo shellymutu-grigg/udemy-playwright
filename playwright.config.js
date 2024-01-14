929365fc8941090f7d177c6d5d49ee89de45f971
shellymutu-grigg <shellymutugrigg@gmail.com>
2024-01-14 19:36:48 +1300

    Section 15 Lesson 74: Configure Jenkins

--- playwright.config.js
+++ playwright.config.js
@@ -4,37 +4,19 @@ const { devices } = require('@playwright/test');
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
-let config = {
+const config = {
   testDir: './tests',
   timeout: 30 * 1000,
+  testMatch: '*/*.spec.js',
   expect: {
     timeout: 5000
   },
   reporter: 'html',
   use: {
-    // Safari = webkit
-    // browserName: 'chromium'
+    browserName : 'chromium',
     headless: false,
     screenshot: 'on', // 'off', 'on', 'only-on-failure'
     trace: 'on', // 'off', 'on', 'retain-on-failure'
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-     // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-  ],
 };
 module.exports = config;
